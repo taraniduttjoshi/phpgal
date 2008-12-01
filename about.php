dd085141e581c59867bf393f8ad59c0d34042271
jonathan.is.not <jonathan.is.not@b96dd228-bf17-11dd-becf-13071112e4e2>
2008-12-01 00:50:52 +0000

    added structure to allow ie

--- about.php
+++ about.php
@@ -1,11 +1,74 @@
-<?php 
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<?php
 $cat=$_GET["cat"];
-
+    require 'conf.php'; 
 if (isset($cat)) {
-	echo '<div id="content">';
-}
-?>
-	<h3 id="cat_tit">about</h3>
+	echo '
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
+	<meta name="author" content="" />
+	<title>'; echo $web_title; echo '</title>
+	
+	<script type="text/javascript" src="js/jquery.pack.js"></script>
+	<script type="text/javascript" src="js/jquery.history.js"></script>
+	<script type="text/javascript" src="js/js.js"></script>
+	
+	<link rel="stylesheet" type="text/css" href="css/fancy.css" media="screen" />
+
+	<script type="text/javascript" src="js/jquery.pngFix.pack.js"></script>
+	<script type="text/javascript" src="js/jquery.fancybox.js"></script>
+
+	<link rel="stylesheet" type="text/css" href="css/style.css" media="screen" />
+
+</head>
+<body>
+
+<div id="all">
+	<div id="constant">
+		<div id="heading">
+			<h1><a href="about.php?cat=theabout">'; echo $web_title; echo '</a></h1>
+			<div id="web_desc">
+				<h2>'; echo $web_desc; echo '</h2>
+			</div>
+		</div>
+			<ul id="nav">
+			<li><a href="about.php?cat=theabout">about</a></li>';
+			
+			function folderlist(){
+			 $startdir = 'gallery/';
+			 $ignoredDirectory[] = '.';
+			 $ignoredDirectory[] = '..';
+			  if (is_dir($startdir)){
+				  if ($dh = opendir($startdir)){
+					  while (($folder = readdir($dh)) !== false){
+						  if (!(array_search($folder,$ignoredDirectory) > -1)){
+							if (filetype($startdir . $folder) == "dir"){
+								  $directorylist[$startdir . $folder]['name'] = $folder;
+								  $directorylist[$startdir . $folder]['path'] = $startdir;
+							  }
+						  }
+					  }
+					  closedir($dh);
+				  }
+			  }
+			return($directorylist);
+			}
+
+			$folders = folderlist();
+			 foreach ($folders as $folder){
+			   $path = $folder['path'];
+			   $name = $folder['name'];
+
+			   echo '<li><a href="category.php?cat=' . $name .'">' . $name . '</a></li>';
+			 }
+echo '
+			
+			</ul>
+	</div><!-- constant -->
+
+<div id="content"> ';}  ?>
+		<h3 id="cat_tit">about</h3>
 	<div id="about">
 		<p>
 		<h3>Welcome!</h3>
@@ -29,6 +92,10 @@ if (isset($cat)) {
 	</div>
 <?php 
 if (isset($cat)) {
-	echo '</div>';
-}
-?>
+	echo '
+</div>
+<div id="wrapper"></div>
+</div><!--end all-->
+
+</body>
+</html>';} ?>
