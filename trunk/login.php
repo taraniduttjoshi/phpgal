1763afebf580166a0332a7b41184836a5466f7f1
jonathan.is.not <jonathan.is.not@b96dd228-bf17-11dd-becf-13071112e4e2>
2008-12-01 02:15:00 +0000

    Added css styling

--- trunk/login.php
+++ trunk/login.php
@@ -2,6 +2,13 @@
 	// we must never forget to start the session
 	session_start();
 
+	if (!isset($_SESSION['adm_in'])
+		|| $_SESSION['adm_in'] == true) {
+
+		header('Location: upload.php');
+		exit;
+	}
+
 	$errorMessage = '';
 	if (isset($_POST['txtUserId']) && isset($_POST['txtPassword'])) {
 		// check if the username and password combination is correct
@@ -17,39 +24,50 @@
 			$errorMessage = 'Sorry, wrong username / password';
 		}
 	}
+    require 'conf.php'; 
 ?>
 	
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
 <head>
 	<title>Login-er @ <?php echo "$web_title"; ?></title>
+	
+	<link rel="stylesheet" href="css/style.css" type="text/css" />
 
 	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
 </head>
 
 <body>
-<?php
-if ($errorMessage != '') {
-?>
-<p align="center"><strong><font color="#990000"><?php echo $errorMessage; ?></font></strong></p>
-<?php
-}
-?>
-<form action="" method="post" name="frmLogin" id="frmLogin">
- <table width="400" border="1" align="center" cellpadding="2" cellspacing="2">
-  <tr>
-   <td width="150">User Id</td>
-   <td><input name="txtUserId" type="text" id="txtUserId"></td>
-  </tr>
-  <tr>
-   <td width="150">Password</td>
-   <td><input name="txtPassword" type="password" id="txtPassword"></td>
-  </tr>
-  <tr>
-   <td width="150">&nbsp;</td>
-   <td><input name="btnLogin" type="submit" id="btnLogin" value="Login"></td>
-  </tr>
- </table>
-</form>
+	
+	<div id="all">
+	<div id="constant">
+		<div id="heading">
+			<h1><a href="about.php?cat=cat"><?php echo "$web_title"; ?></a></h1>
+			<div id="web_desc">
+				<h2><?php echo $web_desc; ?></h2>
+			</div>
+		</div>
+	</div>
+	<div id="content">
+	<h3 id="cat_tit">Login-er</h3>
+		<div id="text">	
+			<?php if ($errorMessage != '') {?>
+				<p align="center"><font color="yellow"><?php echo $errorMessage; ?></font></p>
+			<?php } ?>
+			<form action="" method="post" name="frmLogin" id="frmLogin">	 
+				<p class="form"><label for="author"><small>My Name is(required)</small></label>
+				<input name="txtUserId" type="text" id="txtUserId">
+				</p>
+
+				<p class="form"><label for="email"><small>Password</small></label>
+				<input name="txtPassword" type="password" id="txtPassword">
+				</p>
+				
+				<p class="form" id="submit">
+				<input name="btnLogin" type="submit" id="btnLogin" tabindex="5" value="Login" />
+				</p>
+			</form>
+		</div>
+	</div>
 </body>
 </html>
