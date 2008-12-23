3a5a5fde2d216644ecf5f6b9538e381a58a29d78
jonathan.is.not <jonathan.is.not@b96dd228-bf17-11dd-becf-13071112e4e2>
2008-12-23 21:46:16 +0000

    deleted option for fancybox. ie7 works

--- ajaxify/js/js.js
+++ ajaxify/js/js.js
@@ -1,15 +1,82 @@
 $(document).ready(function() {  
+
+	var title=document.title
 	
+	function fancy(){
+		$("ul#fancy a").fancybox({
+
+		});	
+	}
+
+	$('#content').wrap('<div id="content-wrapper"></div>');
+	fancy()	
+		
 	$('#nav li a').ajaxify({
-	loadHash: true,
-	target: '#content',
-	tagToload: '#ajax'
+		loading_image: 'js/loading.gif',	
+		loadHash: true,
+		target: '#content',
+		tagToload: '#ajax',
+		onStart:
+			function(){
+				$('#content-wrapper').hide('fast');
+				
+				document.title=title;
+
+				$('#wrapper').append('<span id="load">LOADING...</span>');
+				$('#load').fadeIn('normal');
+			},
+		onSuccess:
+			function(){
+				
+				var name = location.hash;
+				var titl = name.substring(18) + ' @ ' + title;
+				document.title=titl;
+				
+				fancy()
+			},
+		onComplete:		
+			function(){
+				setTimeout(function(){
+					$('#content-wrapper').show('normal');
+					$('#load').fadeOut('normal');
+					$('#load').remove();
+				} ,300)
+			}
+
    });
 
 	$('#heading h1 a').ajaxify({
-	loadHash: true,
-	target: '#content',
-	tagToload: '#ajax'
+		loading_image: 'js/loading.gif',	
+		loadHash: true,
+		target: '#content',
+		tagToload: '#ajax',
+		onStart:
+			function(){
+				$('#content-wrapper').hide('fast');
+				
+				document.title=title;
+
+				$('#wrapper').append('<span id="load">LOADING...</span>');
+				$('#load').fadeIn('normal');
+			},
+		onSuccess:
+			function(){
+				
+				var name = location.hash;
+				var titl = name.substring(18) + ' @ ' + title;
+				document.title=titl;
+				
+				fancy()
+			},
+		onComplete:		
+			function(){
+				setTimeout(function(){
+					$('#content-wrapper').show('normal');
+					$('#load').fadeOut('normal');
+					$('#load').remove();
+				} ,300)
+			}
+
    });
 
   }); 
