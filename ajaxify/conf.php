f2b8237c9080f008c2a80ef114dadb7bfcd91d01
jonathan.is.not <jonathan.is.not@b96dd228-bf17-11dd-becf-13071112e4e2>
2008-12-27 05:21:41 +0000

    ars.volce to byVolce

--- ajaxify/conf.php
+++ ajaxify/conf.php
@@ -1,6 +1,6 @@
 <?php
 
-$web_title="Ars.Volce";
+$web_title="byVolce";
 $web_desc="The online portfolio of Jonathan Solichin";
 $path_to_image_directory="gallery/";
 $path_to_tn_directory="gallery/". $cat ."/tn/";
